25ddf0a6a9cb462b6111c356f76767a7c13cb08f
babucarr <babucarr.badjie12@gmail.com>
2025-03-30 20:46:14 +0800

    change the hamburger menu to ellipsis in the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ export function Header(props) {
   return (
     <header>
       <button onClick={handleToggleMenu} className="open-nav-button">
-        <i className="fa-solid fa-bars"></i>
+        <i className="fa-solid fa-ellipsis"></i>
+        {/* <i className="fa-solid fa-bars"></i> */}
       </button>
       <h1 className="text-gradient">Pokédex</h1>
     </header>
